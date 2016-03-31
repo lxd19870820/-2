d7263057bcccf4ae75acec77bddffbb44e2106e1
lxd <lxd@qq.com>
2016-03-31 11:58:32 +0800

    测试2

---
+++ DF/DF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DF
+//
+//  Created by 李修东 on 16/3/31.
+//  Copyright © 2016年 lixiudong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
