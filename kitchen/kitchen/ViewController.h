b97f7a7eb718a34fe98139ca08088b8dde6fa59f
lxd <lxd@qq.com>
2016-03-31 11:54:50 +0800

    hah

---
+++ kitchen/kitchen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kitchen
+//
+//  Created by 李修东 on 16/3/31.
+//  Copyright © 2016年 lixiudong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ kitchen/kitchen/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  kitchen
+//
+//  Created by 李修东 on 16/3/31.
+//  Copyright © 2016年 lixiudong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
